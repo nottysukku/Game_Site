b32f15f384b6e0a44471c8bf552e916c716c7fe2
Sukrit Chopra <109043008+nottysukku@users.noreply.github.com>
2024-08-03 16:17:35 +0530

    Update Button1.jsx

--- src/Button1.jsx
+++ src/Button1.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './Button1.css';
 
 const Button1 = () => {
+  const handleClick = () => {
+    window.location.href = 'https://snake-js-vert.vercel.app/';
+};
   return (
     <div className='w-full h-screen bg-black flex items-center justify-center'>
       <button onClick={handleClick} className="relative text-glow cursor-pointer px-6 py-2 border border-glow rounded-md bg-transparent text-base font-extrabold tracking-widest shadow-glow animate-border-flicker group transform transition-transform duration-500 hover:scale-105">

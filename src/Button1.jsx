4ee56cccdb0781528b7ad8a46de90d5e577a27c3
Sukrit Chopra <109043008+nottysukku@users.noreply.github.com>
2024-08-07 00:48:45 +0530

    Update Button1.jsx

--- src/Button1.jsx
+++ src/Button1.jsx
@@ -6,8 +6,8 @@ const Button1 = () => {
     <div className='w-full h-screen bg-black flex items-center justify-center'>
       <button onClick={()=>{
         window.location.href = 'https://snake-js-vert.vercel.app/';
-      }} className="relative left-24 text-glow cursor-pointer px-6 py-2 border border-glow rounded-md bg-transparent text-base font-extrabold tracking-widest shadow-glow animate-border-flicker group transform transition-transform duration-500 hover:scale-105">
-        <span className=" float-left -mr-4 text-shadow animate-text-flicker">
+      }} className="absolute text-glow cursor-pointer px-6 py-2 border border-glow rounded-md bg-transparent text-base font-extrabold tracking-widest shadow-glow animate-border-flicker group transform transition-transform duration-500 hover:scale-105">
+        <span className="relative float-left -mr-4 text-shadow animate-text-flicker">
           P<span className="faulty-letter animate-faulty-flicker">L</span>AY Snake
         </span>
         <div className="absolute inset-0 opacity-70 blur-md transform translate-y-20 rotate-x-95 scale-y-35 bg-glow pointer-events-none"></div>
@@ -15,7 +15,7 @@ const Button1 = () => {
       </button>
       <div onClick={()=>{
         window.location.href = 'https://game-site-orpin.vercel.app/';
-      }} className='text-white flex relative right-40 top-56  text-glow cursor-pointer px-6 py-2 border border-glow rounded-md bg-transparent text-base font-extrabold tracking-widest shadow-glow animate-border-flicker group transform transition-transform duration-500 hover:scale-105'>Back Home?</div>
+      }} className='text-white flex relative top-56  text-glow cursor-pointer px-6 py-2 border border-glow rounded-md bg-transparent text-base font-extrabold tracking-widest shadow-glow animate-border-flicker group transform transition-transform duration-500 hover:scale-105'>Back Home?</div>
     </div>
   );
 }
